f500fa970fbf2894bc87933c24ceaec729475858
Miles Kenner <mdk0109@yahoo.com>
2019-03-02 18:04:43 -0600

    connection successful

--- bamazon.js
+++ bamazon.js
@@ -2,3 +2,23 @@
 var iquirer = require("inquirer");
 var mysql = require("mysql");
 
+// creating connection to database
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Phyllis21",
+    database: "bamazon"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+  });
